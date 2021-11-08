21ee45928e55f4bd609390dc87cd34d0106fc164
Walter Hinojosa <88406709+YahirH@users.noreply.github.com>
2021-11-08 12:34:26 -0600

    Add files via upload

---
+++ powershellpia.ps1
@@ -0,0 +1,7 @@
+﻿function Ver-Reglasblock {
+    param([Parameter()] [string] $activo = 'True',
+    [Parameter()] [string] $vari = 'Block')
+    Get-NetFirewallRule -Enabled $activo -EdgeTraversalPolicy $vari
+}
+
+Ver-Reglasblock
