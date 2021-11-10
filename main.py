b3644807fd36e6a1e504f41b957a00d6da9a377a
Walter Hinojosa <88406709+YahirH@users.noreply.github.com>
2021-11-09 23:02:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser = argparse.ArgumentParser(description='Usamos una API para el pia')
     parser.add_argument("-op", "--option", dest="op", required=True,
-                        help='Opción A =WebScrapping/Obtención de Metadatos/Envio de Correo con resultado. Opción B =API de revisión de dirección de Correo Opción C = Escaneo de puertos Opción D= Obtener el nombre de Dominio Completo de mi equipo')
+                        help='Opción A =WebScrapping/Obtención de Metadatos/Envio de Correo con resultado. Opción B =API de revisión de dirección de Correo Opción C = Ver Reglas de Bloqueo del Firewall Opción D= Obtener el nombre de Dominio Completo de mi equipo, Opción E=Obtener un Universally Unique IDentifier(UUID) y cambiar nombre a un archivo con la misma')
     parser.add_argument("-u", "--url", dest='url',
                         help="Enter an url to webscrap")
     parser.add_argument("-rec", "--receiver", dest='rec', type=str,
@@ -32,6 +32,10 @@ def main():
                         type=str, help="Enter email para analizar")
     parser.add_argument("-k", "--key", dest='key',
                         type=str, help="APIKey de tu cuenta")
+    parser.add_argument("-arch", "--archivo", dest='arch',
+                        type=str, help="Archivo para cambiar su nombre por un UUID")
+
+    
     args = parser.parse_args()
     x = args.op
 
@@ -52,8 +56,9 @@ def main():
     if x == 'D' or x == 'd':
         funciones.Fdqn()
     if x == 'E' or x == 'e':
-        funciones.identUU()
+        funciones.identUU(args.arch)
 
 
 if __name__ == "__main__":
     main()
+
